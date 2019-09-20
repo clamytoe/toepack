842cf4c1dcbcc32666d26ba811ce9519c779b534
Martin Uribe <clamytoe@gmail.com>
2019-09-20 10:50:04 -0500

    Added logs directory with empty starter logfiles

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/log_init.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/log_init.py
@@ -6,25 +6,23 @@ Initializes logging for the project.
 import os
 import json
 import logging.config
+from pathlib import Path
 
 
-module_path = __file__
-pwd = "/".join(module_path.split("/")[:-1])
-filename = "logging.json"
-log_config = os.path.join(pwd, filename)
+MODULE_PATH = Path(__file__).resolve().parent
+LOG_CONFIG = MODULE_PATH.joinpath("logging.json")
 
 
 def setup_logging(
-    default_path=log_config, default_level=logging.INFO, env_key="LOG_CFG"
+    default_path=LOG_CONFIG, default_level=logging.INFO, env_key="LOG_CFG"
 ):
     """Setup logging configuration"""
     path = default_path
     value = os.getenv(env_key, None)
     if value:
-        path = value
-    if os.path.exists(path):
-        with open(path, "rt") as f:
-            config = json.load(f)
+        path = Path(value)
+    if path.is_file():
+        config = json.loads(path.read_text())
         logging.config.dictConfig(config)
     else:
         logging.basicConfig(level=default_level)
